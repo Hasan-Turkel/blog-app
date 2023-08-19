e31b39d64744aaf5f0c344dfbcb1438c0d924c8e
hasan <mmhotmail>
2023-08-19 12:38:34 +0300

    footer commit

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='w-100 text-center p-3'>
+      <p className='fs-4 fw-bolder'>
+        Developed by Hasan Turkel
+      </p>
+    </footer>
   )
 }
 
