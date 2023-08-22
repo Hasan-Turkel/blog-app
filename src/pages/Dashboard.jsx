a468d229a4c10c6ab089ee6c8ac2be78d9fa12da
hasan <mmhotmail>
2023-08-22 15:47:28 +0300

    privaterouter

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -19,7 +19,7 @@ useEffect(() => {
 
   return (
     <div className="row justify-content-center my-5 gap-4">
-      {data?.map((item)=>(<div key={item.id} getCard={getCard} className="col-9 col-md-4 col-lg-3"><Card   {...item} getCard={getCard} /></div>))}
+      {data?.map((item)=>(<div key={item.id}  className="col-9 col-md-4 col-lg-3"><Card   {...item} getCard={getCard} /></div>))}
     </div>
 
     
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,40 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Login from "../pages/Login"
-import Register from "../pages/Register"
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 // import PrivateRouter from "./PrivateRouter"
-import Dashboard from "../pages/Dashboard"
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-import NewBlog from "../pages/NewBlog"
-import About from "../pages/About"
-import MyBlogs from "../pages/myBlogs"
-import Profile from "../pages/Profile"
-import Detail from "../pages/Detail"
-
+import Dashboard from "../pages/Dashboard";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import NewBlog from "../pages/NewBlog";
+import About from "../pages/About";
+import MyBlogs from "../pages/myBlogs";
+import Profile from "../pages/Profile";
+import Detail from "../pages/Detail";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
     <Router>
-        <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="detail/:id" element={<Detail />} />
         <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login/>} />
-        <Route path="new-blog" element={<NewBlog />} />
+        <Route path="login" element={<Login />} />
         <Route path="about" element={<About />} />
-        <Route path="my-blogs" element={<MyBlogs />} />
-        <Route path="profile" element={<Profile />} />
-        {/* <Route path="" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />}>
-          </Route>
-        </Route>  */}
-         
+        <Route path="" element={<PrivateRouter />}>
+          <Route path="detail/:id" element={<Detail />}></Route>
+          <Route path="new-blog" element={<NewBlog />} />
+          <Route path="my-blogs" element={<MyBlogs />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
